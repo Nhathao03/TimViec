e9e8a570bf54cf40cde1e1c69c599626689dac18
Quang-tuoi-tre <honhatquang890@gmail.com>
2024-04-18 16:24:07 +0700

    ung tuyen

---
+++ TimViec/Migrations/20240418085737_v6.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimViec.Migrations
+{
+    /// <inheritdoc />
+    public partial class v6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "StatusJobs",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "StatusJobs",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StatusJobs_UserId",
+                table: "StatusJobs",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StatusJobs_AspNetUsers_UserId",
+                table: "StatusJobs",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StatusJobs_AspNetUsers_UserId",
+                table: "StatusJobs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StatusJobs_UserId",
+                table: "StatusJobs");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "StatusJobs");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "StatusJobs",
+                newName: "ID");
+        }
+    }
+}
--- TimViec/Models/StatusJob.cs
+++ TimViec/Models/StatusJob.cs
@@ -2,13 +2,16 @@
 {
     public class StatusJob
     {
-        public int ID { get; set; } 
+        public int Id { get; set; } 
         public string Email { get; set; }    
         public string Status { get; set; }
 
         public string Fullname { get; set; }
 
         public string Note { get; set; }
+
+        public ApplicationUser? User { get; set; }
+
 
     }
 }
