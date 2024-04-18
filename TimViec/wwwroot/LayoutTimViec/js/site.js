43eec00aace999b218bae10079fd2b71849eb160
Quang-tuoi-tre <honhatquang890@gmail.com>
2024-04-18 21:04:36 +0700

    hố le

--- TimViec/wwwroot/LayoutTimViec/js/site.js
+++ TimViec/wwwroot/LayoutTimViec/js/site.js
@@ -2,7 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const myBtn = document.getElementById('myBtn');
+const myOverlay = document.getElementById('myOverlay');
+const closeBtn = document.getElementById('closeBtn');
 
 function translateEN() {
     window.location.href = 'https://translate.google.com/translate?h1=en&s1=vi&t1=en&u=${window.location.href}';
 }
+
+//myBtn.addEventListener('click', function () {
+//    myOverlay.style.display = 'block'; // Hiển thị overlay
+//});
+
+//closeBtn.addEventListener('click', function () {
+//    myOverlay.style.display = 'none'; // Ẩn overlay
+//});
